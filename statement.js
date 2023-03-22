00d8e9a62a793ede5de9535fbcef38886b7b29d7
Chris Leech <chrisleech@monzo.com>
2023-03-22 15:18:01 +0000

    Refactored statement to get passing tests

--- statement.js
+++ statement.js
@@ -1,17 +1,21 @@
 class Statement {
-  static printStatement(transactions, balance) {
-    let header;
-    header = "date || credit || debit || balance";
-    transactions.length === 0 ? header + "\n" : header;
-    const rows = transactions.map((transaction) => {
-      const amount = transaction.amount.toFixed(2);
-      const date = transaction.date;
-      const credit = transaction.type === "credit" ? amount : "";
-      const debit = transaction.type === "debit" ? amount : "";
-      const balance = transaction.balance.toFixed(2);
-      return `${date} || ${credit} || ${debit} || ${balance}`;
+  // Refactored statement to only take transactions as an argument
+  // to avoid calculating balance in the statement class
+  static printStatement(transactions) {
+    const header = "date || credit || debit || balance";
+    const rows = transactions.reverse().map(({ date, amount, balance }) => {
+      let credit = "";
+      let debit = "";
+      if (amount > 0) {
+        credit = amount.toFixed(2);
+      } else {
+        debit = Math.abs(amount).toFixed(2);
+      }
+      const paddedBalance = balance !== undefined ? balance.toFixed(2) : "";
+      console.log(paddedBalance);
+      return `${date} || ${credit} || ${debit} || ${paddedBalance}`;
     });
-    return header + rows.reverse().join("\n");
+    return header + (rows.length > 0 ? "\n" + rows.join("\n") : "");
   }
 }
 
--- statement.test.js
+++ statement.test.js
@@ -10,11 +10,11 @@ describe("Statement class", () => {
   it("can print a statement when transactions have happened", () => {
     const balance = 2500;
     const transactions = [
-      { date: "10/01/2012", amount: 1000, type: "credit", balance: 1000 },
-      { date: "13/01/2012", amount: 2000, type: "credit", balance: 3000 },
-      { date: "14/01/2012", amount: 500, type: "debit", balance: 2500 },
+      { date: "10/01/2012", amount: 1000, balance: 1000 },
+      { date: "13/01/2012", amount: 2000, balance: 3000 },
+      { date: "14/01/2012", amount: -500, balance: 2500 },
     ];
-    expect(Statement.printStatement(transactions, balance)).toEqual(
+    expect(Statement.printStatement(transactions)).toEqual(
       "date || credit || debit || balance\n14/01/2012 ||  || 500.00 || 2500.00\n13/01/2012 || 2000.00 ||  || 3000.00\n10/01/2012 || 1000.00 ||  || 1000.00"
     );
   });
